8fafcd84a7cbb4030fddbd3addeef7b6484c9dbf
Daniel <kuanshenwang@gmail.com>
2023-12-12 15:30:58 +0800

    Add Room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -44,4 +45,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Room
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    // To use Kotlin annotation processing tool
+    ksp("androidx.room:room-compiler:$room_version")
+    // To use Kotlin Symbol Processing
+    ksp("androidx.room:room-compiler:$room_version")
 }
