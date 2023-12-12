32b7da24bcef7b7a620ce060ecb4db4ec2903c6a
Daniel <kuanshenwang@gmail.com>
2023-12-12 16:05:19 +0800

    Add note table name

--- app/src/main/java/com/freelanxer/ktroomdb/db/entity/NoteEntity.kt
+++ app/src/main/java/com/freelanxer/ktroomdb/db/entity/NoteEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "table_note")
 data class NoteEntity(
     @PrimaryKey
     val noteId: String?,
